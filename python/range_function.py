d5a2e664ca333e563e23703dad7da09deb74d202
Nick Batlle <nmb436@gmail.com>
2017-02-14 13:37:37 -0800

    new file

---
+++ python/range_function.py
@@ -0,0 +1,16 @@
+my_list = [0,1,2,3]
+my_list_len = len(my_list)
+for i in range(4):
+    print(my_list[i])
+    
+
+my_list = [0,1,2,3]
+my_list_len = len(my_list)
+for i in range(3,-1,-1):
+    print(my_list[i])
+
+
+my_list = [0,1,2,3,4,5,6,7,8,9,10]
+my_list_len = len(my_list)
+for i in range(8,0,-2):
+    print(my_list[i])
