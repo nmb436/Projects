5cca2e70d6adbff8d6f4d32d17572e3540728038
Nick Batlle <nmb436@gmail.com>
2017-02-16 17:01:25 -0800

    file transfer project file

---
+++ file_transfer.py
@@ -0,0 +1,20 @@
+import shutil
+shutil.copytree('C:/Users/Student/Desktop/a', 'C:/Users/Student/Desktop/b/a')
+shutil.rmtree('C:/Users/Student/Desktop/b/a')
+shutil.move('C:/Users/Student/Desktop/a/berry.txt','C:/Users/Student/Desktop/b')
+print("C:/Users/Student/Desktop/a/berry.txt")
+shutil.move('C:/Users/Student/Desktop/a/orange.txt','C:/Users/Student/Desktop/b')
+print("C:/Users/Student/Desktop/a/orange.txt")
+shutil.move('C:/Users/Student/Desktop/a/apple.txt','C:/Users/Student/Desktop/b')
+print("C:/Users/Student/Desktop/a/apple.txt")
+shutil.move('C:/Users/Student/Desktop/a/lemon.txt','C:/Users/Student/Desktop/b')
+print("C:/Users/Student/Desktop/a/lemon.txt")
+
+
+
+
+
+
+
+
+
